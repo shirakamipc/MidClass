8e1fcc56fdc481c482ef6a3197697270af883316
Bùi Hoàng Tú <bhtu.work@gmail.com>
2022-05-23 23:51:51 +0700

    add .sql to support login and sign up

---
+++ dbo.Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+	[UId]      Int           Identity(1,1),
+    [UName]    NVARCHAR (50) Unique,
+    [Mail]     NCHAR (50)    NOT NULL,
+    [Password] NCHAR (50)    NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UId] ASC)
+);
+
