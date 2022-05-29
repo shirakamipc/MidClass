17e0cdb2e62b57a268e5e0d9e6d6a38f0f9f1f7d
Bùi Hoàng Tú <bhtu.work@gmail.com>
2022-05-29 20:11:39 +0700

    AP is done remove unnecessary element

--- AllProducts.cs
+++ AllProducts.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,34 +11,43 @@ using System.Windows.Forms;
 
 namespace MidClass
 {
-	public partial class AllProducts : Form
+	public partial class Allproducts : Form
 	{
-		public AllProducts()
+		public Allproducts()
 		{
 			InitializeComponent();
 		}
+		#region Virables
+		DBAccess dBAccess = new DBAccess();
+		public static DataTable dtProducts = new DataTable();
+		String query;
+		Byte[] ImageByteArray;
+		#endregion
 
-		private void productBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+		private void Allproducts2_Load(object sender, EventArgs e)
 		{
+			query = "select * from EndClass.dbo.Product";
+			dBAccess.readDatathroughAdapter(query, dtProducts);
+			productsGrid.DataSource = dtProducts;
+			productsGrid.Columns[4].Visible = true;
+			productPicture.Image = new Bitmap(@"..\..\no_img.png");
+			for (int i = 0; i <= productsGrid.Columns.Count - 1; i++)
+			{
+				productsGrid.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+			}
 
+			dBAccess.closeConn();
 		}
 
-		private void productBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+		//this is in fact update products
+		private void UpdateProducts_Click(object sender, EventArgs e)
 		{
-			this.Validate();
-			this.productBindingSource.EndEdit();
-			this.tableAdapterManager.UpdateAll(this.dSProducts);
-
+			query = "select * from EndClass.dbo.Product";
+			int changes = dBAccess.executeDataAdapter(dtProducts, query);
+			MessageBox.Show("There is " + changes + " changes");
 		}
 
-		private void AllProducts_Load(object sender, EventArgs e)
-		{
-			// TODO: This line of code loads data into the 'dSProducts.Product' table. You can move, or remove it, as needed.
-			this.productTableAdapter.Fill(this.dSProducts.Product);
-
-		}
-
-		private void productDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		private void productsGrid_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
 		{
 			if (e.ColumnIndex == 4)
 			{
@@ -47,18 +56,40 @@ namespace MidClass
 				if (ofd.ShowDialog() == DialogResult.OK)
 				{
 					String strFilePath = ofd.FileName;
-					//pbxImage.Image = new Bitmap(strFilePath);
-					if (imgText.Text.Trim().Length == 0)//Auto-Fill title if is empty
-						imgText.Text = System.IO.Path.GetFileName(strFilePath);
+					productPicture.Image = new Bitmap(strFilePath);
+					if (imgTitle.Text.Trim().Length == 0)//Auto-Fill title if is empty
+						imgTitle.Text = System.IO.Path.GetFileName(strFilePath);
 
 					// convert bitmap to jpeg
 					Image temp = new Bitmap(strFilePath);
 					MemoryStream strm = new MemoryStream();
 					temp.Save(strm, System.Drawing.Imaging.ImageFormat.Jpeg);
-					Byte[] ImageByteArray = strm.ToArray();
-
-					//save to database
+					ImageByteArray = strm.ToArray();
+					//to datatable
+					if (dtProducts.Rows.Count <e.RowIndex +1)
+					{
+						dtProducts.Rows.Add();
+						MessageBox.Show("new row");
+					}
+					dtProducts.Rows[e.RowIndex].SetField(4, ImageByteArray);
 				}
+			}
+		}
+
+		private void productsGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			if (e.ColumnIndex == 4)
+			{
+				if (dtProducts.Rows.Count >= e.RowIndex + 1) 
+					if (dtProducts.Rows[e.RowIndex][4] == DBNull.Value)
+					{
+						productPicture.Image = new Bitmap(@"..\..\no_img.png");
+					}
+					else
+					{
+						ImageByteArray = (byte[])dtProducts.Rows[e.RowIndex][4];
+						productPicture.Image = (Bitmap)((new ImageConverter()).ConvertFrom(ImageByteArray)); ;
+					}
 			}
 		}
 	}
